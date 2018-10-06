00b2dd9f18d8f01a9f52514e7d4dd853fe7fc0fe
Andrew <32229265+aselee@users.noreply.github.com>
2018-10-06 16:19:46 -0500

    adding album

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import HeaderBar from './components/Header';
 import './App.css';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import HeaderBar from './components/Header';
+import Main from './components/Main';
+import Album from './components/Album';
+
 
 class Webportfolio extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <div className="App">
-          <HeaderBar />
-        </div>
-      </React.Fragment>
+      <MuiThemeProvider>
+        <React.Fragment>
+          <CssBaseline/>
+            <div className="App">
+              <HeaderBar />
+              <Main />
+              <Album />
+            </div>
+        </React.Fragment>
+      </MuiThemeProvider>
     );
   }
 };
