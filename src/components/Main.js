b61a3473ce7a096a8ca237e5e3b1349f31f1f2df
Andrew <32229265+aselee@users.noreply.github.com>
2018-10-06 16:20:32 -0500

    testing album

---
+++ src/components/Main.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+
+const styles = theme => ({
+  mainFeaturedPost: {
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing.unit * 4,
+  },
+  mainFeaturedPostContent: {
+    padding: `${theme.spacing.unit * 6}px`,
+  },
+  inputCenter: {
+    textAlign: "center",
+  },
+})
+
+function Main(props){
+  const { classes } = props;
+
+  return(
+    <Paper className={classes.mainFeaturedPost}>
+      <Grid container>
+        <Grid item md={6}>
+          <div className={classes.mainFeaturedPostContent}>
+            <Typography variant="display2" color="inherit" gutterBottom>
+              Full-Stack JavaScript Developer
+            </Typography>
+            <Typography variant="headline" color="inherit" paragraph>
+              Hi, my name is Andrew! I'm a inspiring JavaScript Web Developer and 
+              looking for opportunities to enhance my skills. 
+              Please take a look at my portfolio and leave a message!
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
+    </Paper>
+  )
+};
+
+export default withStyles(styles)(Main);
