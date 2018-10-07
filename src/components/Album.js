1cd25a09129e1ec4deffaedead3594fbbb4f9f8d
Andrew <32229265+aselee@users.noreply.github.com>
2018-10-07 18:28:44 -0500

    Update

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { withStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  icon: {
+    marginRight: theme.spacing.unit * 2,
+  },
   heroUnit: {
     backgroundColor: theme.palette.background.paper,
   },
@@ -19,6 +25,9 @@ const styles = theme => ({
     maxWidth: 600,
     margin: '0 auto',
     padding: `${theme.spacing.unit * 8}px 0 ${theme.spacing.unit * 6}px`,
+  },
+  heroButtons: {
+    marginTop: theme.spacing.unit * 4,
   },
   layout: {
     width: 'auto',
@@ -29,25 +38,28 @@ const styles = theme => ({
       marginLeft: 'auto',
       marginRight: 'auto',
     },
-    cardGrid: {
-      padding: `${theme.spacing.unit * 8}px 0`,
-    },
-    card: {
-      height: '100%',
-      display: 'flex',
-      flexDirection: 'column',
-    },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
-    cardContent: {
-      flexGrow: 1,
-    },
+  },
+  cardGrid: {
+    padding: `${theme.spacing.unit * 8}px 0`,
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing.unit * 6,
   },
 });
 
-const cards = [1, 2, 3, 4, 5, 6];
-
+const cards = [1, 2, 3];
 
 function Album(props) {
   const { classes } = props;
@@ -55,16 +67,20 @@ function Album(props) {
   return (
     <React.Fragment>
       <CssBaseline />
-        <main>
-          <div className={classes.heroUnit}>
-            <div className={classes.heroContent}>
-              <Typography variant="display3" align="center" color="textPrimary" gutterBottom>
-                Projects
-              </Typography>
-              <Typography variant="title" align="center" color="textSecondary" paragraph>
-                Here are some of my projects that I am currently working on.
-              </Typography>
-          <div className={classNames(classes.layout, classes.cardGrid)}>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroUnit}>
+          <div className={classes.heroContent}>
+            <Typography variant="display3" align="center" color="textPrimary" gutterBottom>
+              Projects
+            </Typography>
+            <Typography variant="title" align="center" color="textSecondary" paragraph>
+              Current projects
+            </Typography>
+          </div>
+        </div>
+        <div className={classNames(classes.layout, classes.cardGrid)}>
+          {/* End hero unit */}
           <Grid container spacing={40}>
             {cards.map(card => (
               <Grid item key={card} sm={6} md={4} lg={3}>
@@ -94,16 +110,15 @@ function Album(props) {
               </Grid>
             ))}
           </Grid>
-          </div>
-          </div>
-          </div>
-        </main>
+        </div>
+      </main>
     </React.Fragment>
-  )
-};
+  );
+}
 
 Album.propTypes = {
   classes: PropTypes.object.isRequired,
 };
+
 
 export default withStyles(styles)(Album);
