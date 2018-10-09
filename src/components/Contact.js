6d2fa8278078d97ba2fd17aac07119aed0125be5
Andrew <32229265+aselee@users.noreply.github.com>
2018-10-09 01:26:34 -0500

    initial

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Divider from '@material-ui/core/Divider';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Cards from '@material-ui/core/Card';
+
+
+
+// export default class InfoBox extends Component {
+//   render() {
+//     const DividerExampleForm = () => (
+//       <div>
+//         </div>
+//     return DividerExampleForm();
+//   }
+// }
